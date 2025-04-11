8b0147044cd4b41eec90a1a73ff7eda6e8f12bc0
Pawel Gnat <pawel_gnat@o2.pl>
2025-04-11 19:17:59 +0200

    chore: add default user

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,6 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEFAULT_USER_ID = "a8f8513f-8617-45b9-b5e1-0b8944a4fb51";
