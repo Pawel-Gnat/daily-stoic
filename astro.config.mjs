c9707a9edff70577527f7a3134d4579471155ad2
Pawel Gnat <pawel_gnat@o2.pl>
2025-05-05 10:29:29 +0200

    cicd: add main workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: cloudflare(),
   integrations: [react(), sitemap()],
-  server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
 });
