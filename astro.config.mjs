60feebb43ba6837cc3c94bfd9774d162bd159c0d
Pawel Gnat <pawel_gnat@o2.pl>
2025-05-05 10:36:09 +0200

    build: add complile to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,9 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
